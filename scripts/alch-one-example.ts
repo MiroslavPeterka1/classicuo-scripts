9095489c6f90ab2419c67623ce2aa7afdac508d3
Miroslav Peterka <39192258+MiroslavPeterka1@users.noreply.github.com>
2026-09-20 23:12:01 +0200

    Add alch-one-example script for potion mixing

---
+++ scripts/alch-one-example.ts
@@ -0,0 +1,17 @@
+const mortar = client.findType(0xE9B, 0xB87, player.backpack);
+if (!mortar) { 
+  client.headMsg(`Nemáš hmoždíř`, player.serial, 0x25);
+  exit(`Nemáš hmoždíř`);
+}
+
+journal.clear();
+player.use(mortar.serial);
+const data = Gump.findOrWait(0x2B1);
+data.horizontalMenuSelect(0xF09, 0x0);
+journal.waitForText("You completed the mixture in the mortar","Ackerman", 50000);
+const lahve = client.findType(0xF0E, 0x0, player.backpack);
+if (!lahve) { 
+  client.headMsg(`Nemáš lahve`, player.serial, 0x25);
+  exit(`Nemáš lahve`);
+}
+player.use(mortar.serial);
