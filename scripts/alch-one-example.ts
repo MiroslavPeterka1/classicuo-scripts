332240df76db267fe6951bdbe71116c7a74b6b72
Miroslav Peterka <39192258+MiroslavPeterka1@users.noreply.github.com>
2026-09-22 10:43:19 +0200

    Update waitForText to use null for speaker

--- scripts/alch-one-example.ts
+++ scripts/alch-one-example.ts
@@ -8,7 +8,7 @@ journal.clear();
 player.use(mortar.serial);
 const data = Gump.findOrWait(0x2B1);
 data.horizontalMenuSelect(0xF09, 0x0);
-journal.waitForText("You completed the mixture in the mortar","Ackerman", 50000);
+journal.waitForText("You completed the mixture in the mortar",null , 50000);
 const lahve = client.findType(0xF0E, 0x0, player.backpack);
 if (!lahve) { 
   client.headMsg(`Nemáš lahve`, player.serial, 0x25);
